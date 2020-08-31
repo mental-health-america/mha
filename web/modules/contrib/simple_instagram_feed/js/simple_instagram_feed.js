ad485da8956ca4063dea77c7314e3374df019bdc
Bradley Waye <bradleywaye@Bradleys-MBP-2.hsd1.ga.comcast.net>
2020-08-31 16:03:30 -0400

    simple instagram feed

--- web/modules/contrib/simple_instagram_feed/js/simple_instagram_feed.js
+++ web/modules/contrib/simple_instagram_feed/js/simple_instagram_feed.js
@@ -2,7 +2,14 @@
   'use strict';
   Drupal.behaviors.simple_instagram_feed = {
     attach: function (context, settings) {
-      var simple_instagram_feed_array = Object.values(drupalSettings.simple_instagram_feed);
+      // Avoid sending multiple requests to the Instagram.
+      if (context !== document) {
+        return;
+      }
+      var simple_instagram_feed_array = Object.keys(drupalSettings.simple_instagram_feed).map(function (e) {
+        return drupalSettings.simple_instagram_feed[e]
+      })
+
       for (var i = 0; i < simple_instagram_feed_array.length; i++) {
         var instagram_username = simple_instagram_feed_array[i].instagram_username;
         var display_profile = simple_instagram_feed_array[i].display_profile;
@@ -13,19 +20,19 @@
         var block_target = (simple_instagram_feed_array[i].block_without_id === 'true' ?
           '.block-' + block_instance : '#block-' + block_instance);
         block_target = block_target + ' .instagram-feed';
-        //Verify items per row
+        // Verify items per row.
         var items_per_row;
         if (simple_instagram_feed_array[i].items_per_row_type == 0) {
           items_per_row = simple_instagram_feed_array[i].items_per_row_default;
         } else {
-          var screenWidth = $( window ).width();
+          var screenWidth = $(window).width();
           if (screenWidth < 720) {
             items_per_row = simple_instagram_feed_array[i].items_per_row_l_720;
-          }
-          else if(screenWidth >= 720 && screenWidth < 960) {
+          } else if (screenWidth >= 720 && screenWidth < 960) {
             items_per_row = simple_instagram_feed_array[i].items_per_row_l_960;
-          }else
+          } else {
             items_per_row = simple_instagram_feed_array[i].items_per_row_h_960;
+          }
         }
 
         var feed_settings = {
@@ -39,9 +46,11 @@
           items: items,
           margin: 0.25
         };
+        
         if (styling) {
           feed_settings.items_per_row = items_per_row;
         }
+
         $.instagramFeed(feed_settings, context);
 
         $(window, context).resize(function () {
@@ -51,13 +60,13 @@
             if (simple_instagram_feed_array[i].items_per_row_type == 1) {
               if (screenWidth < 720) {
                 width = (100 - 0.5 * 2 * simple_instagram_feed_array[i].items_per_row_l_720) / simple_instagram_feed_array[i].items_per_row_l_720;
-              }
-              else if (screenWidth >= 720 && screenWidth < 960) {
+              } else if (screenWidth >= 720 && screenWidth < 960) {
                 width = (100 - 0.5 * 2 * simple_instagram_feed_array[i].items_per_row_l_960) / simple_instagram_feed_array[i].items_per_row_l_960;
-              } else
+              } else {
                 width = (100 - 0.5 * 2 * simple_instagram_feed_array[i].items_per_row_h_960) / simple_instagram_feed_array[i].items_per_row_h_960;
+              }
               var block_id = "block-" + simple_instagram_feed_array[i].block_instance;
-              $("#" + block_id + " img", context).width(width+"%");
+              $("#" + block_id + " img", context).width(width + "%");
             }
           }
         });
