fccf3f4cf5d17f94e89e59afb335550092aadc84
Bradley Waye <bradleywaye@Bradleys-MBP-2.hsd1.ga.comcast.net>
2020-08-31 16:06:06 -0400

    colorbox inline

--- web/modules/contrib/colorbox_inline/js/colorbox_inline.js
+++ web/modules/contrib/colorbox_inline/js/colorbox_inline.js
@@ -16,11 +16,22 @@
             href: data.colorboxInline,
             width: data.width,
             height: data.height,
-            rel: data.rel
+            rel: data.rel,
+            open: false
           });
+
+		// Hide the colorbox source after if it was hidden before use.
+        if (!$(settings.href).filter(':visible')[0]) {
+          settings.onClosed = function($link) {
+            $($link.cache.href).hide();
+          }
+        }
+
         $link.colorbox(settings);
-        $link.colorbox.close();
-        $link.click(function () { $(this).colorbox();});
+        $link.click(function () {
+         $($(this).data('colorboxInline')).show();
+         $(this).colorbox();
+        });
       });
     }
   };
