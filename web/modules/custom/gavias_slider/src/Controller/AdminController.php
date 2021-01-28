a4cc82f9f9d6f12b3e31693c9a8d7a847589074d
Brad Waye <brad.waye@gmail.com>
2021-01-28 12:18:44 -0500

    update theme mods

--- web/modules/custom/gavias_slider/src/Controller/AdminController.php
+++ web/modules/custom/gavias_slider/src/Controller/AdminController.php
@@ -16,13 +16,13 @@ class AdminController extends ControllerBase {
 
   public function gavias_slider_list(){
   
-    if(!db_table_exists('gavias_slider')){
+    if(!\Drupal::database()->schema()->tableExists('gavias_slider')){
       return "";
     }
 
     $header = array( 'ID', 'Name', 'Action');
     
-    $results = db_select('{gavias_slider}', 'd')
+    $results = \Drupal::database()->select('{gavias_slider}', 'd')
             ->fields('d', array('id', 'name'))
             ->execute();
     $rows = array();
@@ -33,10 +33,10 @@ class AdminController extends ControllerBase {
       $tmp[] = $row->id;
       $tmp[] = $row->name;
       $tmp[] = t('<a href="@link_1">Edit Name</a> | <a href="@link_2">Edit Silder</a> | <a href="@link_3">Config Global</a> | <a href="@link_4">Delete</a>', array(
-            '@link_1' => \Drupal::url('gavias_slider.admin.add', array('sid' => $row->id)),
-            '@link_2' => \Drupal::url('gavias_slider.admin.edit', array('sid' => $row->id)),
-            '@link_3' => \Drupal::url('gavias_slider.admin.config', array('sid' => $row->id)),
-            '@link_4' => \Drupal::url('gavias_slider.admin.delete', array('sid' => $row->id))
+            '@link_1' =>  Url::fromRoute('gavias_slider.admin.add', array('sid' => $row->id))->toString(),
+            '@link_2' =>  Url::fromRoute('gavias_slider.admin.edit', array('sid' => $row->id))->toString(),
+            '@link_3' =>  Url::fromRoute('gavias_slider.admin.config', array('sid' => $row->id))->toString(),
+            '@link_4' =>  Url::fromRoute('gavias_slider.admin.delete', array('sid' => $row->id))->toString()
         ));
       $rows[] = $tmp;
     }
@@ -44,7 +44,7 @@ class AdminController extends ControllerBase {
       '#theme' => 'table',
       '#header' => $header,
       '#rows' => $rows,
-      '#empty' => t('No Slider available. <a href="@link">Add Slider</a>.', array('@link' => \Drupal::url('gavias_slider.admin.add', array('sid'=>0)))),
+      '#empty' => t('No Slider available. <a href="@link">Add Slider</a>.', array('@link' => Url::fromRoute('gavias_slider.admin.add', array('sid'=>0))->toString())),
     );
   }
 
@@ -53,7 +53,7 @@ class AdminController extends ControllerBase {
     $page['#attached']['library'][] = 'gavias_slider/gavias_slider.assets.admin';
     $slideshow = gavias_slider_load($sid);
     
-    $abs_url_config = \Drupal::url('gavias_slider.admin.save', array(), array('absolute' => FALSE));
+    $abs_url_config = Url::fromRoute('gavias_slider.admin.save', array(), array('absolute' => FALSE))->toString();
     $page['#attached']['drupalSettings']['gavias_slider']['base_url'] = $base_url;
     $page['#attached']['drupalSettings']['gavias_slider']['saveConfigURL'] = $abs_url_config;
     $page['#attached']['drupalSettings']['gavias_slider']['setting'] = json_encode($slideshow->settings);
@@ -74,7 +74,7 @@ class AdminController extends ControllerBase {
     $page['#attached']['library'][] = 'gavias_slider/gavias_slider.assets.admin';
     $slideshow = gavias_slider_load($sid);
     
-    $abs_url_config = \Drupal::url('gavias_slider.admin.save', array(), array('absolute' => FALSE));
+    $abs_url_config = Url::fromRoute('gavias_slider.admin.save', array(), array('absolute' => FALSE))->toString();
     $page['#attached']['drupalSettings']['gavias_slider']['base_url'] = $base_url;
     $page['#attached']['drupalSettings']['gavias_slider']['saveConfigURL'] = $abs_url_config;
     $page['#attached']['drupalSettings']['gavias_slider']['settings'] = $slideshow->settings;
@@ -98,12 +98,12 @@ class AdminController extends ControllerBase {
     $settings = $_REQUEST['settings'];
     $action = $_REQUEST['action'];
     if($action == 'config'){
-      db_update('{gavias_slider}')->fields(array(
+      \Drupal::database()->update('{gavias_slider}')->fields(array(
         'settings' => $settings
       ))->condition('id', $sid, '=')->execute();
     }
     if($action == 'slide'){
-      db_update('{gavias_slider}')->fields(array(
+      \Drupal::database()->update('{gavias_slider}')->fields(array(
           'data' => $data,
       ))->condition('id', $sid, '=')->execute();
     }  
