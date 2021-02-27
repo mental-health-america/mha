34eb8a2a3ad3da7810d7d56e8f563d44075bfda8
Brad Waye <brad.waye@gmail.com>
2021-02-27 13:41:55 -0500

    peer support email mod

--- web/modules/custom/form_general_email/src/Plugin/Block/form_general_email.php
+++ web/modules/custom/form_general_email/src/Plugin/Block/form_general_email.php
@@ -23,16 +23,9 @@ class form_general_email extends BlockBase {
   public function build() {
     return [
       '#markup' => $this->t('
-<div id="xXPxbAInLU" class="container">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                            <div class="mx-auto clearfix area">
-    <script type="text/javascript" src="https://default.salsalabs.org/api/widget/template/6a21d121-b5f3-4486-bd56-e8efe1b95ba5/?tId=xXPxbAInLU" ></script>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+      <div id="xXPxbAInLU">
+        <script type="text/javascript" src="https://default.salsalabs.org/api/widget/template/6a21d121-b5f3-4486-bd56-e8efe1b95ba5/?tId=xXPxbAInLU" ></script>
+      </div>
       '),
     ];
 
---
+++ web/modules/custom/form_peer_support_email/src/Plugin/Block/form_peer_support_email.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ */
+
+namespace Drupal\form_peer_support_email\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a Custom Block
+ * @Block(
+ * id = "block_form_peer_support_email",
+ * admin_label = @Translation("Form Peer Support Email Sign up"),
+ * )
+ */
+class form_peer_support_email extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#markup' => $this->t('
+<div class="row">
+  <div class="col">
+    <div class="p-5 red-background rounded-corners clearfix">
+      <div id="hFwZMqrqDw">
+        <script type="text/javascript" src="https://default.salsalabs.org/api/widget/template/08ac95d1-0980-430e-a5f3-3350eaec4285/?tId=hFwZMqrqDw" ></script>
+      </div>
+     </div>
+  </div>
+</div>
+      '),
+    ];
+
+  }
+
+}
+
+
