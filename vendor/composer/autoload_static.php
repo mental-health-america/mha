e42b3217d0c288c2041b62ba2bd36af5bd14cbe1
Brad Waye <bradwaye@comm-bwaye3.local>
2022-10-15 14:43:30 -0400

    update mods

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -573,9 +573,9 @@ class ComposerStaticInit6a304bd6fcf54197feff320618fdafbc
         'Doctrine\\Common\\' => 
         array (
             0 => __DIR__ . '/..' . '/doctrine/common/lib/Doctrine/Common',
-            1 => __DIR__ . '/..' . '/doctrine/event-manager/lib/Doctrine/Common',
-            2 => __DIR__ . '/..' . '/doctrine/persistence/lib/Doctrine/Common',
-            3 => __DIR__ . '/..' . '/doctrine/reflection/lib/Doctrine/Common',
+            1 => __DIR__ . '/..' . '/doctrine/persistence/lib/Doctrine/Common',
+            2 => __DIR__ . '/..' . '/doctrine/reflection/lib/Doctrine/Common',
+            3 => __DIR__ . '/..' . '/doctrine/event-manager/src',
         ),
         'Consolidation\\SiteProcess\\' => 
         array (
