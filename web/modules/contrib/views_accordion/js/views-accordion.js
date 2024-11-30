d85f8f86ad5073c4005e2f333de0133d5c782df4
Bradley Waye <bradleywaye@users.noreply.github.com>
2024-11-30 10:51:51 -0500

    update views accordion

--- web/modules/contrib/views_accordion/js/views-accordion.js
+++ web/modules/contrib/views_accordion/js/views-accordion.js
@@ -2,48 +2,46 @@
  * @file
  */
 
-(function ($, Drupal, drupalSettings) {
-  'use strict';
+(($, Drupal, drupalSettings) => {
   Drupal.behaviors.views_accordion = {
-    attach: function (context) {
+    attach() {
       if (drupalSettings.views_accordion) {
-        $.each(drupalSettings.views_accordion, function (id) {
-
-          var $display = $(this.display + ':not(.ui-accordion)');
+        $.each(drupalSettings.views_accordion, function processViews() {
+          const $display = $(`${this.display}:not(.ui-accordion)`);
 
           /* The row count to be used if Row to display opened on start is set to random */
-          var row_count = 0;
+          let rowCount = 0;
 
           /* Prepare our markup for jquery ui accordion */
-          $(this.header, $display).each(function (i) {
+          $(this.header, $display).each(function processHeader() {
             // Wrap the accordion content within a div if necessary.
             if (!this.usegroupheader) {
               $(this).siblings().wrapAll('<div></div>');
-              row_count++;
+              rowCount += 1;
             }
           });
 
-          if (this.rowstartopen == 'random') {
-            this.rowstartopen = Math.floor(Math.random() * row_count);
+          if (this.rowstartopen === 'random') {
+            this.rowstartopen = Math.floor(Math.random() * rowCount);
           }
 
           // The settings for the accordion.
-          var accordionSettings = {
+          const accordionSettings = {
             header: this.header,
             animate: {
-              'easing': this.animated,
-              'duration': parseInt(this.duration),
+              easing: this.animated,
+              duration: parseInt(this.duration, 10),
             },
             active: this.rowstartopen,
             collapsible: this.collapsible,
             heightStyle: this.heightStyle,
             event: this.event,
-            icons: false
+            icons: false,
           };
           if (this.useHeaderIcons) {
             accordionSettings.icons = {
-              'header': this.iconHeader,
-              'activeHeader': this.iconActiveHeader
+              header: this.iconHeader,
+              activeHeader: this.iconActiveHeader,
             };
           }
 
@@ -51,6 +49,6 @@
           $display.accordion(accordionSettings);
         });
       }
-    }
+    },
   };
 })(jQuery, Drupal, drupalSettings);
